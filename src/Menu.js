330af14bd18795f9ed76d8c14684059dd1661e81
Olya <ponomaryova.olya.y@gmail.com>
2023-02-21 13:19:13 +0100

    Add menu. Update manifest.json

--- src/Menu.js
+++ src/Menu.js
@@ -4,27 +4,56 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
 
 export default function ButtonAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <div>
+            <Button
+              id="fade-button"
+              aria-controls={open ? "fade-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              color="secondary"
+              size="large"
+            >
+              <MenuIcon />
+            </Button>
+            <Menu
+              id="fade-menu"
+              MenuListProps={{
+                "aria-labelledby": "fade-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              TransitionComponent={Fade}
+            >
+              <MenuItem>Creativity</MenuItem>
+              <MenuItem>Gratitude </MenuItem>
+              <MenuItem>Yoga</MenuItem>
+            </Menu>
+          </div>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Name of the Journal
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Settings</Button>
         </Toolbar>
       </AppBar>
     </Box>
